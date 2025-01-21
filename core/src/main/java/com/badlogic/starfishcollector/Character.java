f9c909f255d5b2f232eef2bd5fb5d488ab073436
Iliev Artem <aartemiliev@gmail.com>
2025-01-21 18:58:35 +0200

    Added Orc for loosing game also

--- core/src/main/java/com/badlogic/starfishcollector/Character.java
+++ core/src/main/java/com/badlogic/starfishcollector/Character.java
@@ -2,14 +2,17 @@ package com.badlogic.starfishcollector;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
-public class Character extends ActorBeta{
-    private final float characterSpeed = 5.0f;
-    public Character () {
+
+public class Character extends ActorBeta {
+    private final float normalSpeed = 2.5f;
+    private final float sprintSpeed = 5.0f;
+
+    public Character() {
         super();
     }
 
-    public void act(float dt)
-    {
+    @Override
+    public void act(float dt) {
         super.act(dt);
 
         float screenWidth = Gdx.graphics.getWidth();
@@ -18,28 +21,25 @@ public class Character extends ActorBeta{
         float newX = getX();
         float newY = getY();
 
-//        if (Gdx.input.isKeyPressed(Keys.LEFT) || Gdx.input.isKeyPressed(Keys.A))
-//            this.moveBy(-characterSpeed,0);
-//        if (Gdx.input.isKeyPressed(Keys.RIGHT) || Gdx.input.isKeyPressed(Keys.D))
-//            this.moveBy(characterSpeed,0);
-//        if (Gdx.input.isKeyPressed(Keys.UP) || Gdx.input.isKeyPressed(Keys.W))
-//            this.moveBy(0,characterSpeed);
-//        if (Gdx.input.isKeyPressed(Keys.DOWN) || Gdx.input.isKeyPressed(Keys.S))
-//            this.moveBy(0,-characterSpeed);
+        float currentSpeed = Gdx.input.isKeyPressed(Keys.SHIFT_LEFT) ? sprintSpeed : normalSpeed;
 
+        // Рух персонажа
         if (Gdx.input.isKeyPressed(Keys.LEFT) || Gdx.input.isKeyPressed(Keys.A))
-            newX -= characterSpeed;
+            newX -= currentSpeed;
         if (Gdx.input.isKeyPressed(Keys.RIGHT) || Gdx.input.isKeyPressed(Keys.D))
-            newX += characterSpeed;
+            newX += currentSpeed;
         if (Gdx.input.isKeyPressed(Keys.UP) || Gdx.input.isKeyPressed(Keys.W))
-            newY += characterSpeed;
+            newY += currentSpeed;
         if (Gdx.input.isKeyPressed(Keys.DOWN) || Gdx.input.isKeyPressed(Keys.S))
-            newY -= characterSpeed;
+            newY -= currentSpeed;
 
-        if (newX < 0) newX = 0; // Ліва межа
+        // Обмеження руху в межах екрану
+        if (newX < 0) newX = 0;
         if (newX + getWidth() > screenWidth) newX = screenWidth - getWidth();
-        if (newY < 0) newY = 0; // Нижня межа// Верхня межа
+        if (newY < 0) newY = 0;
+        if (newY + getHeight() > screenHeight) newY = screenHeight - getHeight();
 
+        // Оновлення позиції персонажа
         setPosition(newX, newY);
     }
 }
--- core/src/main/java/com/badlogic/starfishcollector/ZoroCollectorBeta.java
+++ core/src/main/java/com/badlogic/starfishcollector/ZoroCollectorBeta.java
@@ -8,33 +8,58 @@ public class ZoroCollectorBeta extends GameBeta {
     private ActorBeta zoro;
     private ActorBeta grass;
     private ActorBeta winMessage;
+    private ActorBeta orc;
+    private ActorBeta gameOverMessage;
     private boolean win;
 
     public void initialize() {
+        // Додавання текстур
         grass = new ActorBeta();
-        grass.setTexture(new Texture(Gdx.files.internal("grass.png")));
+        grass.setTexture(new Texture(Gdx.files.internal("grass-1.png")));
         mainStage.addActor(grass);
+
         zoro = new ActorBeta();
         zoro.setTexture(new Texture(Gdx.files.internal("zoro-1.png")));
         zoro.setPosition(380, 380);
         mainStage.addActor(zoro);
+
         mainCharacter = new Character();
-        mainCharacter.setTexture(new Texture(Gdx.files.internal("main-character-1.png")));
+        mainCharacter.setTexture(new Texture(Gdx.files.internal("character-2.png")));
         mainCharacter.setPosition(20, 20);
         mainStage.addActor(mainCharacter);
+
         winMessage = new ActorBeta();
         winMessage.setTexture(new Texture(Gdx.files.internal("you-win.png")));
         winMessage.setPosition(180, 180);
         winMessage.setVisible(false);
         mainStage.addActor(winMessage);
+
+        orc = new ActorBeta();
+        orc.setTexture(new Texture(Gdx.files.internal("orc-1.png")));
+        orc.setPosition(200, 200);
+        mainStage.addActor(orc);
+
+        gameOverMessage = new ActorBeta();
+        gameOverMessage.setTexture(new Texture(Gdx.files.internal("game-over.png")));
+        gameOverMessage.setPosition(150, 180);
+        gameOverMessage.setVisible(false);
+        mainStage.addActor(gameOverMessage);
+
         win = false;
     }
 
     public void update(float dt) {
-        if (mainCharacter.overlaps(zoro))
-        {
+        // Перевірка, чи персонаж торкнувся зорро
+        if (mainCharacter.overlaps(zoro)) {
             zoro.remove();
             winMessage.setVisible(true);
+            win = true;
+        }
+
+        // Перевірка, чи персонаж торкнувся орка
+        if (mainCharacter.overlaps(orc)) {
+            mainCharacter.remove(); // Видалення персонажа зі сцени
+            gameOverMessage.setVisible(true); // Відображення повідомлення "Game Over"
         }
     }
 }
